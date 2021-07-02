500e61d6784b97e8ccf1f22c8dbb2fa37c67712c
hayatul11 <hayatulemon340@gmail.com>
2021-07-02 11:09:26 +0600

    variable and datatypes

---
+++ datatypes.js
@@ -0,0 +1,38 @@
+// primitive datatypes
+// Number
+// // 10 2 3.4 5.7 3.44 
+
+// String 
+// 'this is a test' + 'this is also text';
+
+// Boolean 
+// true false
+
+// undefined
+// null
+
+// object datatypes
+// Array
+
+
+
+
+// ****** NUMBERS ******
+
+var n = 125;
+var float = 4.55;
+var nn = Number(45);
+var nn = Number('95.77');
+console.log(nn);
+
+console.log(Number.parseFloat(nn));
+console.log(Number.parseInt(nn));
+
+console.log(Number.MIN_VALUE)
+console.log(Number.MAX_VALUE);
+
+console.log(1 / 0); //infinity
+console.log('abc' * 10) // NaN
+
+
+// ****** STRING ******
